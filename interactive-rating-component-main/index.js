e83f3812e86d6e39e19c936bb0392f17df67768f
Ebube <aaronebube123@gmail.com>
2022-05-01 16:42:19 +0100

    some bug fixes

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -1,18 +1,17 @@
 const form = document.forms.rating;
-
 const inputCheck = document.querySelectorAll('input[type="radio"]');
 const labels = document.querySelectorAll("label");
 const mainForm = document.querySelector(".main");
 const thankform = document.querySelector(".thanks");
 const answer = document.querySelector(".answer");
 
-console.log(thankform.classList.replace("display", "null"));
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	inputCheck.forEach((item) => {
 		if (item.checked) {
 			console.log(item.value);
 			answer.textContent = item.value;
+			thankform.classList = "main thanks";
 			mainForm.classList.add("display");
 		}
 	});
